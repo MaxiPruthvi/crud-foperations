534c21564389d199b952733dbda2a916067d5859
MaxiPruthvi <maxipruthviraj@gmail.com>
2024-10-28 15:22:10 +0530

    third commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import mysql.connector
+import os
 
 app = Flask(__name__)
 
@@ -82,4 +83,5 @@ def employee(id):
     conn.close()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
